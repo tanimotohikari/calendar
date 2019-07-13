def2d91903f2d8adc647416966ea64cdf123af97
tanimotohikari <tanimoto.hikari@gmail.com>
2019-07-13 11:16:51 +0900

    選択処理の修正

--- script/calendar.js
+++ script/calendar.js
@@ -1,20 +1,23 @@
 $(function() {
   const weeks = ['日', '月', '火', '水', '木', '金', '土'];
-  const date = new Date();
   const $prev = $('#js-btn-prev');
   const $next = $('#js-btn-next');
   const $calendar = $('#js-calendar');
   const $calendarTitle = $('#js-calendar-title');
+
+  const date = new Date();
   const thisYear = date.getFullYear(); //本日の日付を取得
   const thisMonth = date.getMonth() + 1; //本日の日付を取得
-  const thisDay = date.getDay(); //本日の日付を取得
-
-  let selectedDays = []; // 選択した希望日を格納する配列
+  const thisDay = date.getDate(); //本日の日付を取得
   let currentYear = date.getFullYear();  // 年の取得
   let currentMonth = date.getMonth() + 1; // 返される値は 0～11なので+1する
   let lastMonthEndDate = new Date(currentYear, currentMonth - 1, 0); // 前月の最後の日
   let lastMonthendDayCount = lastMonthEndDate.getDate(); // 前月の末日
   let calendarTitle = `${ currentYear }年${ currentMonth }月`;
+  let selectDateStatus = false; // デフォルトの日付から選択された場合にtrueに変更される
+
+  // ▼カレンダーを複数選択方式にするときは下記で定義した配列を使用する
+  // let selectedDays = []; // 選択した希望日を格納する配列
 
   // ▼new Date()で第一引数に年、第二引数に月、第三引数に-1で月の最初,0で月の最後を取得できる
   // うるう年も簡単に判定できる　
@@ -61,23 +64,26 @@ $(function() {
 
     for (let w = 0; w < 6; w++) {
       calendarHtml += '<tr>';
-
       for (let d = 0; d < 7; d++) {
         if (w == 0 && d < startDay) {
           // 1行目で1日の曜日の前
           let num = lastMonthendDayCount - startDay + d + 1;
-          calendarHtml += `<td class='text-disable is-disabled' data-month='${ prevMonth }' data-year='${ currentYear }'>${ num }</td>`;
+          calendarHtml += `<td class='is-disabled'>${ num }</td>`;
         } else if (dayCount > endDayCount) {
           // 末尾の日数を超えた場合
           let num = dayCount - endDayCount;
-          calendarHtml += `<td class='text-disable is-disabled' data-month='${ nextMonth }' data-year='${ currentYear }'>${ num }</td>`;
+          calendarHtml += `<td class='is-disabled'>${ num }</td>`;
           dayCount++;
-        } else if (thisDay >= dayCount - 2 && thisMonth === currentMonth) {
-          // 本日より前の日程は選択できないようにする※本日も含めた日から
-          calendarHtml += `<td class='text-disable is-disabled' data-month='${currentMonth}' data-year='${currentYear}'>${dayCount}</td>`;
+        } else if (thisDay + 2 > dayCount && thisMonth === currentMonth) {
+          // 本日より前の日程は選択できないようにする
+          calendarHtml += `<td class='is-disabled'>${dayCount}</td>`;
           dayCount++;
         } else {
-          calendarHtml += `<td data-month='${ currentMonth }' data-year='${ currentYear }'>${ dayCount }</td>`;
+          if (thisDay + 2 === dayCount && thisMonth === currentMonth && !selectDateStatus) {
+            calendarHtml += `<td class='is-selected' data-month='${ currentMonth }' data-year='${ currentYear }'>${ dayCount }</td>`;
+          } else {
+            calendarHtml += `<td data-month='${ currentMonth }' data-year='${ currentYear }'>${ dayCount }</td>`;
+          }
           dayCount++;
         }
       }
@@ -109,50 +115,62 @@ $(function() {
   }
 
   function selectDay() {
+    console.log('hoho');
     let selectDay = Number($(this).text());
     let selectDate = ([currentYear, currentMonth, selectDay]).toString();
-
     $('#js-calendar-table td').removeClass('is-selected');
     $(this).addClass('is-selected');
-
-    // ここの処理は今回の仕様ではいらないのでコメントアウト
-    // if(selectedDays.indexOf(selectDate) === -1) {
-    //   // 配列に同じ数値が入ってなかったら追加する
-    //   selectedDays.push(selectDate);
-    //   $(this).addClass('is-selected');
-    // } else {
-    //   // 配列に同じ数値が入っていた場合は削除する
-    //   let arrayIndex = selectedDays.indexOf(selectDate);
-    //   selectedDays.splice(arrayIndex, 1); 
-    //   $(this).removeClass('is-selected');
-    // }
-    // 配列の中身を昇順に並び替える うまくできてない
-    // selectedDays = selectedDays.sort();
+    selectDateStatus = true;
   }
 
   function checkAlreadySelect() {
-    // カレンダーを別の月に切り替えた時に選択されたものがリセットされる
-    // 複数選択方式から単一選択に変えたため
-    if (selectedDays.length) {
-      let length = selectedDays.length;
-      let values = [];
-      for (let index = 0; index < length; index++) {
-        values = selectedDays[index];
-        values = values.split(',');
-        $('#js-calendar-table').find('td').each(function(index, element) {
-          let $element = $(element);
-          // 日にちだけデータ型がstringなので数値に変換する
-          let year = $element.data('year');
-          let month = $element.data('month');
-          let day = Number($element.text());
-          values[index] = Number(values[index]);
-          if (year === values[0] && month === values[1] && day === values[2]) {
-            $element.addClass('is-selected');
-          }
-        });
-      }
-    }
+    console.log('hoge');
   }
+
+  // ▼カレンダーを複数選択方式にするときは下記の処理を使用する
+  // function selectDay() {
+  //   let selectDay = Number($(this).text());
+  //   let selectDate = ([currentYear, currentMonth, selectDay]).toString();
+
+  //   $('#js-calendar-table td').removeClass('is-selected');
+  //   $(this).addClass('is-selected');
+
+  //   if(selectedDays.indexOf(selectDate) === -1) {
+  //     // 配列に同じ数値が入ってなかったら追加する
+  //     selectedDays.push(selectDate);
+  //     $(this).addClass('is-selected');
+  //   } else {
+  //     // 配列に同じ数値が入っていた場合は削除する
+  //     let arrayIndex = selectedDays.indexOf(selectDate);
+  //     selectedDays.splice(arrayIndex, 1); 
+  //     $(this).removeClass('is-selected');
+  //   }
+  //   配列の中身を昇順に並び替える うまくできてない
+  //   selectedDays = selectedDays.sort();
+  // }
+
+  // function checkAlreadySelect() {
+  //   if (selectedDays.length) {
+  //     let length = selectedDays.length;
+  //     let values = [];
+  //     for (let index = 0; index < length; index++) {
+  //       values = selectedDays[index];
+  //       values = values.split(',');
+  //       $('#js-calendar-table').find('td').each(function(index, element) {
+  //         let $element = $(element);
+  //         // 日にちだけデータ型がstringなので数値に変換する
+  //         let year = $element.data('year');
+  //         let month = $element.data('month');
+  //         let day = Number($element.text());
+  //         values[index] = Number(values[index]);
+  //         if (year === values[0] && month === values[1] && day === values[2]) {
+  //           $element.addClass('is-selected');
+  //         }
+  //       });
+  //     }
+  //   }
+    
+  // }
 
   function showNextBtn() {
     //ボタンの表示非表示について考える
